b9a2495d8e2a62e2f01be11e02de58ee44c836d8
Matt <46137770+xMdb@users.noreply.github.com>
2021-12-28 22:47:14 +1000

    style: format code

--- src/client/Client.ts
+++ src/client/Client.ts
@@ -7,10 +7,10 @@ import { Event } from '../interfaces/Event';
 import { EventEmitter } from 'stream';
 
 class Bot extends Client {
-    public logger = consola;
-    public hook = new WebhookClient({ url: process.env.WEBHOOK_URL as string });
-    
-    constructor() {
+	public logger = consola;
+	public hook = new WebhookClient({ url: process.env.WEBHOOK_URL as string });
+
+	constructor() {
 		super({
 			intents: [Intents.FLAGS.GUILD_MESSAGES],
 		});
@@ -18,41 +18,41 @@ class Bot extends Client {
 		this.start().catch(this.logger.error);
 	}
 
-    private async loadEvents(dir = '../events', emitter: EventEmitter) {
-        const files = await fs.readdir(path.join(__dirname, dir));
+	private async loadEvents(dir = '../events', emitter: EventEmitter) {
+		const files = await fs.readdir(path.join(__dirname, dir));
 
-        for (const file of files) {
-            const stat = await fs.lstat(path.join(__dirname, dir, file));
+		for (const file of files) {
+			const stat = await fs.lstat(path.join(__dirname, dir, file));
 
-            if (stat.isDirectory()) {
-                await this.loadEvents(path.join(dir, file), emitter);
-            } else {
-                if (!(file.endsWith('.ts') || file.endsWith('.js'))) continue;
-                try {
-                    const { name, run } = (await import(path.join(__dirname, dir, file))) as Event;
+			if (stat.isDirectory()) {
+				await this.loadEvents(path.join(dir, file), emitter);
+			} else {
+				if (!(file.endsWith('.ts') || file.endsWith('.js'))) continue;
+				try {
+					const { name, run } = (await import(path.join(__dirname, dir, file))) as Event;
 
-                    if (!name) {
-                        console.warn(`The event ${path.join(__dirname, dir, file)} doesn't have a name!`);
-                        continue;
-                    }
+					if (!name) {
+						console.warn(`The event ${path.join(__dirname, dir, file)} doesn't have a name!`);
+						continue;
+					}
 
-                    if (!(run && typeof run === 'function')) {
-                        console.warn(`The event ${name} doesn't have an executable function!`);
-                        continue;
-                    }
+					if (!(run && typeof run === 'function')) {
+						console.warn(`The event ${name} doesn't have an executable function!`);
+						continue;
+					}
 
-                    emitter.on(name, run.bind(null, this));
-                } catch (e: any) {
-                    console.warn(`Error while loading events: ${e.message}`);
-                }
-            }
-        }
-    }
+					emitter.on(name, run.bind(null, this));
+				} catch (e: any) {
+					console.warn(`Error while loading events: ${e.message}`);
+				}
+			}
+		}
+	}
 
-    private async start() {
-        await this.loadEvents('../events', this);
-        await this.login(process.env.BOT_TOKEN);
-    }
+	private async start() {
+		await this.loadEvents('../events', this);
+		await this.login(process.env.BOT_TOKEN);
+	}
 }
 
 export default Bot;
