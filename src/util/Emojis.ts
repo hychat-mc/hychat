23c882e81965d09a2d2eb21b0cd61026868f9058
Matt <46137770+xMdb@users.noreply.github.com>
2021-12-28 22:56:22 +1000

    revert: restore emojis

---
+++ src/util/Emojis.ts
@@ -0,0 +1,228 @@
+export default {
+	/**
+	 * Join Emoji
+	 */
+	join: '📥',
+
+	/**
+	 * Leave Emoji
+	 */
+	leave: '📤',
+
+	/**
+	 * VIP Rank
+	 */
+	vip: {
+		/* First part */
+		first: '',
+
+		/* Second part */
+		second: '',
+
+		/* Third part */
+		third: '',
+	},
+
+	/**
+	 * VIP+ Rank
+	 */
+	vipPlus: {
+		/* First part */
+		first: '',
+
+		/* Second part */
+		second: '',
+
+		/* Third part */
+		third: '',
+	},
+
+	/**
+	 * MVP Rank
+	 */
+	mvp: {
+		/* First part */
+		first: '',
+
+		/* Second part */
+		second: '',
+
+		/* Third part */
+		third: '',
+	},
+
+	/**
+	 * MVP+ Rank
+	 */
+	mvpPlus: {
+		/* First part */
+		first: '',
+
+		/* Second part */
+		second: '',
+
+		/* Plus Colors */
+		plus: {
+			/* Black */
+			black: '',
+
+			/* Dark Blue */
+			darkBlue: '',
+
+			/* Dark Green */
+			darkGreen: '',
+
+			/* Dark Aqua */
+			darkAqua: '',
+
+			/* Dark Red */
+			darkRed: '',
+
+			/* Dark Purple */
+			darkPurple: '',
+
+			/* Gold */
+			gold: '',
+
+			/* Gray */
+			gray: '',
+
+			/* Dark Gray */
+			darkGray: '',
+
+			/* Blue */
+			blue: '',
+
+			/* Green */
+			green: '',
+
+			/* Aqua */
+			aqua: '',
+
+			/* Red */
+			red: '',
+
+			/* Light Purple */
+			lightPurple: '',
+
+			/* Yellow */
+			yellow: '',
+
+			/* White */
+			white: '',
+		},
+	},
+
+	/**
+	 * MVP++ Rank
+	 */
+	mvpPlusPlus: {
+		/* First part */
+		first: '',
+
+		/* Second part */
+		second: '',
+
+		/* First Plus Colors */
+		firstPlus: {
+			/* Black */
+			black: '',
+
+			/* Dark Blue */
+			darkBlue: '',
+
+			/* Dark Green */
+			darkGreen: '',
+
+			/* Dark Aqua */
+			darkAqua: '',
+
+			/* Dark Red */
+			darkRed: '',
+
+			/* Dark Purple */
+			darkPurple: '',
+
+			/* Gold */
+			gold: '',
+
+			/* Gray */
+			gray: '',
+
+			/* Dark Gray */
+			darkGray: '',
+
+			/* Blue */
+			blue: '',
+
+			/* Green */
+			green: '',
+
+			/* Aqua */
+			aqua: '',
+
+			/* Red */
+			red: '',
+
+			/* Light Purple */
+			lightPurple: '',
+
+			/* Yellow */
+			yellow: '',
+
+			/* White */
+			white: '',
+		},
+
+		/* Second Plus Colors */
+		secondPlus: {
+			/* Black */
+			black: '',
+
+			/* Dark Blue */
+			darkBlue: '',
+
+			/* Dark Green */
+			darkGreen: '',
+
+			/* Dark Aqua */
+			darkAqua: '',
+
+			/* Dark Red */
+			darkRed: '',
+
+			/* Dark Purple */
+			darkPurple: '',
+
+			/* Gold */
+			gold: '',
+
+			/* Gray */
+			gray: '',
+
+			/* Dark Gray */
+			darkGray: '',
+
+			/* Blue */
+			blue: '',
+
+			/* Green */
+			green: '',
+
+			/* Aqua */
+			aqua: '',
+
+			/* Red */
+			red: '',
+
+			/* Light Purple */
+			lightPurple: '',
+
+			/* Yellow */
+			yellow: '',
+
+			/* White */
+			white: '',
+		},
+	},
+};
