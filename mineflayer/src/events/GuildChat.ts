705b477e8a6dea749afdcf65991b2cb810ee1fad
Matt <46137770+xMdb@users.noreply.github.com>
2022-01-05 20:55:58 +1000

    feat(mineflayer): make guild chat up to spec
    Co-authored-by: Jeje <69335633+Jejebecarte@users.noreply.github.com>

--- mineflayer/src/events/GuildChat.ts
+++ mineflayer/src/events/GuildChat.ts
@@ -1,16 +1,23 @@
+import { Util } from 'discord.js';
+
 import { Execute } from '../interfaces/Event';
 import { ChatMessage } from 'prismarine-chat';
 
 export const name = 'chat:guildChat';
 
 export const run: Execute = async (bot, message: ChatMessage) => {
-	// ${Util.escapeMarkdown(message)}
-	const splitMessageArray = message.toString().split(',');
-	console.log(splitMessageArray);
-	if (splitMessageArray[2] === bot.mineflayer.username) return;
-	console.log('EVENT FIRED');
-	const formattedMessage = `channel: ${splitMessageArray[0]} rank: **${splitMessageArray[1] ?? ''} name: ${
-		splitMessageArray[2]
-	}${splitMessageArray[3] ?? ''}**: message: ${splitMessageArray[4]}`;
-	splitMessageArray[0] === 'Guild' ? bot.chatHook.send(formattedMessage) : bot.officerChatHook.send(formattedMessage);
+	const messageArray: string[] = message.toString().split(',');
+
+	const channel = messageArray[0] as 'Guild' | 'Officer';
+	const hypixelRank = messageArray[1] as string | null;
+	const playerName = messageArray[2] as string;
+	const guildRank = messageArray[3] as string | null;
+	const chatMessage = messageArray[4] as string;
+
+	if (playerName === bot.mineflayer.username) return;
+
+	const formattedMessage = `${hypixelRank + ' ' ?? ''}${playerName}${' ' + guildRank ?? ''}: ${Util.escapeMarkdown(
+		chatMessage,
+	)}`;
+	channel === 'Guild' ? bot.chatHook.send(formattedMessage) : bot.officerChatHook.send(formattedMessage);
 };
--- mineflayer/src/interfaces/Chat.ts
+++
@@ -1,9 +0,0 @@
-import { Message } from "discord.js";
-
-export default interface GuildChat extends Array<Message> {
-    channel: 'Guild' | 'Officer',
-	hypixelRank: string | null,
-	playerName: string,
-	guildRank: string | null,
-	message: string,
-}
