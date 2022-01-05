e022cec50a7c2254dfdd559307e4374f607615c8
John Doe <69335633+Jejebecarte@users.noreply.github.com>
2022-01-05 20:13:35 +1100

    fix(patchy-patch): remove unnecessary constructors for Bot class

--- mineflayer/src/index.ts
+++ mineflayer/src/index.ts
@@ -5,7 +5,7 @@ config();
 // Initialize the bot class
 import Bot from './classes/Bot';
 // Normally the webhook would be passed by discord bot
-const bot = new Bot(process.env.WEBHOOK_URL as string, process.env.OFFICER_WEBHOOK_URL as string);
+const bot = new Bot();
 
 // Export the bot to use elsewhere
 export default bot;
