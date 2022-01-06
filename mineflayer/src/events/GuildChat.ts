db3488e0c24973a352da6abc1c4745f1d44cb7f9
Matt <46137770+xMdb@users.noreply.github.com>
2022-01-06 10:20:40 +1000

    fix: remove custom emoji

--- mineflayer/src/events/GuildChat.ts
+++ mineflayer/src/events/GuildChat.ts
@@ -16,7 +16,7 @@ export const run: Execute = async (bot, message: ChatMessage) => {
 
 	if (playerName === bot.mineflayer.username) return;
 
-	const formattedMessage = `<:cryingsunglasses:790636659125125190> ${hypixelRank ?? ''}${playerName}${' ' + guildRank ?? ''}: ${Util.escapeMarkdown(
+	const formattedMessage = `${hypixelRank ?? ''}${playerName}${' ' + guildRank ?? ''}: ${Util.escapeMarkdown(
 		chatMessage,
 	)}`;
 	channel === 'Guild' ? await bot.chatHook.send(formattedMessage) : await bot.officerChatHook.send(formattedMessage);
