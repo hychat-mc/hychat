3a9ed7b5b8361b1d36acd582a58a7b34a50b2235
Matt <46137770+xMdb@users.noreply.github.com>
2022-01-06 22:31:56 +1000

    style: change message of CommentBlocked

--- mineflayer/src/events/CommentBlocked.ts
+++ mineflayer/src/events/CommentBlocked.ts
@@ -4,12 +4,13 @@ export const name = 'chat:commentBlocked';
 
 export const run: Execute = async (bot, message) => {
 	const messageArray: string[] = message.toString().split(',');
+
 	const comment = messageArray[0] as string;
 	const reason = messageArray[1] as string;
 
-	bot.logger.log(`Comment blocked: ${comment}`);
-	bot.devHook.send(`New Comment Blocked: ${comment}`);
-    bot.officerChatHook.send(
-		`A comment, ${comment} was blocked because ${reason}. Developers have been notified of this, however, it is not guaranteed that they will take action on this. Developers will not take responsibility for banned accounts.`,
+	bot.logger.warn(`Comment blocked: ${comment} (${reason})`);
+	bot.devHook.send(`New Comment Blocked: **${comment}**\nReason: **${reason}**`);
+	bot.officerChatHook.send(
+		`The "${comment}" was blocked by Hypixel because "${reason}". Developers will not take responsibility for banned accounts.`,
 	);
 };
