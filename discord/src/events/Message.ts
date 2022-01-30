6ba190499551bf0671aff4090253c1668f4d25c8
Matt <46137770+xMdb@users.noreply.github.com>
2022-01-30 10:20:52 +1000

    refactor: delete discord folder

--- discord/src/events/Message.ts
+++
@@ -1,9 +0,0 @@
-import { Execute } from '../interfaces/Event';
-import { Message } from 'discord.js';
-import Bot from '../client/Client';
-
-export const name = 'messageCreate';
-
-export const run: Execute = async (bot: Bot, message: Message) => {
-    
-};
--- discord/src/index.ts
+++
@@ -1,8 +0,0 @@
-// Load environment variables from the .env file
-import { config } from 'dotenv';
-config();
-
-import Bot from './client/Client.js';
-const bot = new Bot();
-
-export default bot;
